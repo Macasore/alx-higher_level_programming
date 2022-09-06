e3bf0974759038b842d0570cdd3d666397403e0c
Macasore <macasorekingdavid@gmail.com>
2022-09-05 20:46:52 -0700

    converts inserted string to uppercase without the use of inbuilt functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if i == " ":
-            print(" ", end="")
-
         elif ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
