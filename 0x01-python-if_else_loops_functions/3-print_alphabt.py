c50be3b95f3e55d7aa78c415b9016bf744c26b32
Macasore <macasorekingdavid@gmail.com>
2023-03-11 00:02:45 +0100

    prints alphabets without two letters

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-for number in range(97, 122):
+for number in range(97, 123):
     if number == 101 or number == 113:
         continue
     print("{:c}".format(number), end="")
