361ef007e7a8f0a3c5680012987d01add59a30ca
Macasore <macasorekingdavid@gmail.com>
2023-03-11 21:53:56 +0100

    checks for lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print(ord("a") >= 65 and ord("a") <= 90)
+print(ord("z"))
+def islower(c):
+    if (ord(c) >= 97 and ord(c) <= 122):
+        return True
+    else:
+        return False
