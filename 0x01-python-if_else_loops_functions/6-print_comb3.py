1ece6d7d123d027f1428ca878ce17b344d8c056f
Macasore <macasorekingdavid@gmail.com>
2023-03-11 21:37:51 +0100

    prints the possible combination of two numbers in python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if ((i % 10) - (i //10)) > 0:
+    if ((i % 10) - (i // 10)) > 0:
         if i == 89:
             print("{}".format(i))
         else:
