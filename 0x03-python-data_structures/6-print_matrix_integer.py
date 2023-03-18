1aaf44de2a3fa4fb03cf066112ea41af2ace7c69
Macasore <macasorekingdavid@gmail.com>
2023-03-18 14:35:05 +0100

    prints a matix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
-        print()
+        if len(i) == 0:
+            print("")
+        for j, item in enumerate(i):
+            if j != len(i) - 1:
+                print("{:d}".format(item), end=" ")
+            else:
+                print("{:d}".format(item))
