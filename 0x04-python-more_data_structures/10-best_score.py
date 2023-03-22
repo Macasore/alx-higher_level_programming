c44d3a06187d01e466a5258e6d1487479be7f4c9
Macasore <macasorekingdavid@gmail.com>
2023-03-23 00:25:09 +0100

    returns the key with the highest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or {}:
         return None
     else:
         maxvalue = max(a_dictionary.values())
