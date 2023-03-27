62c0d7560942bc8dd7d45afed509c4b5c114b624
Macasore <macasorekingdavid@gmail.com>
2023-03-27 20:05:08 +0100

    a function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print()
+            len = 0
+            for i in my_list:
+                len += 1
+            return len
+    print()
+    return x
