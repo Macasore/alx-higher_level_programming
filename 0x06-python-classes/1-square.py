87dd1cf023173de1b9dedbedbcc4641af12e64a6
Macasore <macasorekingdavid@gmail.com>
2023-03-30 10:08:48 +0100

    defines a square class with private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" No module imported"""
+
+
+class Square:
+    """
+    This class defines a square of private attribute 'size'
+    """
+    def __init__(self, size):
+        """
+        This function initializes the class with a size of type int
+
+        Args:
+        size (int) : the size of the square
+        """
+        self.__size = size
