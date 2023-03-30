ac0b253ea482d0ea4f7e51a49587cc7d0262e816
Macasore <macasorekingdavid@gmail.com>
2023-03-30 20:10:09 +0100

    defines a square class with a private attribute and a public method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class definition of a square
+
+"""
+
+
+class Square:
+    """
+    This is a class that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        This function initializes a square object with a private attribute
+        'size'and returns a Typeerror if it isn't an integer and
+        a ValueError if it's less than zero
+
+        Args:
+             size (int): size of a square
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+
+        This function returns the area of a square
+
+        """
+        return self.__size * self.__size
