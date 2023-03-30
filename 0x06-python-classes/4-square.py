1d84de8438b242f046483fc2ed8804bf01ed57a6
Macasore <macasorekingdavid@gmail.com>
2023-03-30 21:27:01 +0100

    defines a square with a getter and setter method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+"""4-square module
+
+This module contains a class definition of a square
+with a private size attribute
+
+"""
+
+
+class Square:
+    """This class contains the definition of a square
+
+
+    """
+
+    def __init__(self, size=0):
+        """Initializes the square class
+
+        Args:
+             size (int): the size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """	getter for size attribute
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter for size attribute
+
+        Args:
+             value (int): the size of the square
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+
+        This function returns the area of a square
+
+        """
+        return self.__size * self.__size
