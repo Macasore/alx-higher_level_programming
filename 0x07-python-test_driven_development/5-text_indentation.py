6014e112f68944b29e61ed343eeab4a11337b4c2
Macasore <macasorekingdavid@gmail.com>
2023-04-06 14:51:01 +0100

    feat: seperates a string based on some special characters

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Text indentation module
+
+This module contains a function that seperates a string based on
+some special characters
+
+"""
+
+
+def text_indentation(text):
+    """text_indentation function
+
+    This function prints a text with two new lines after
+    some special caracters
+
+    Special characters - '.', '?', ':'
+
+    Args:
+       text (str): the text to be printed
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i, t in enumerate(text):
+        if t in ['.', ':', "?"]:
+            print(t)
+            print()
+            continue
+        elif (t == " " and text[i-1] in ['.', ':', "?"]):
+            continue
+        print(t, end="")
