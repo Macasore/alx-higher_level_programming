a954a0f92520b66a81156e93f2f1003a9e004e01
Macasore <macasorekingdavid@gmail.com>
2023-04-06 05:46:35 +0100

    everything is an object

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" copy list module
+
+This module contains a function that returns a copy of a list
+"""
+
+
+def copy_list(l):
+    """Copy_list function
+
+    This function returns a copy of a list
+
+    """
+    a = l[:]
+    return a
