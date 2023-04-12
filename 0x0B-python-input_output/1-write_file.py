7c000c467c218a6cd9268dfc7cb3e87954a4f17c
Macasore <macasorekingdavid@gmail.com>
2023-04-13 00:05:40 +0100

    feat: writes a string to a text and returns the number of characters written

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1-write_file module"""
+
+
+def write_file(filename="", text=""):
+    """write_file function
+   this is a function that writes a string to a text file
+   and returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        return f.tell()
