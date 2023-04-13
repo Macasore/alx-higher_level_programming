f53ec2a013807b05df853c9bbaa67c8ae68886fe
Macasore <macasorekingdavid@gmail.com>
2023-04-13 13:32:24 +0100

    converts python object to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" to json modlue"""
+import json
+
+
+def to_json_string(my_obj):
+    """to json stting function
+
+    this function converts python objects to json
+    """
+    return json.dumps(my_obj)
