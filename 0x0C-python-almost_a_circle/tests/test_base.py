45447f4d2abe5329321e3d59b3d00942bcd3eb96
Macasore <macasorekingdavid@gmail.com>
2023-04-17 21:27:31 +0100

    fix: add test cases

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -4,13 +4,16 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_idnone(self):
+        """test_id function
+        Tests that the logic for id works as
+        expected i.e initialises object with id if specified
+        or defaults to number of instances with default ids
+        """
         b1 = Base()
         b2 = Base(None)
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-
-    def test_idvalue(self):
         b3 = Base(12)
         b5 = Base(-5)
         self.assertEqual(b3.id, 12)
         self.assertEqual(b5.id, -5)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
