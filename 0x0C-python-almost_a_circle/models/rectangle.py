d8fdfc81da9612912746fa01dfc73e0b42f72b99
Macasore <macasorekingdavid@gmail.com>
2023-04-17 23:14:32 +0100

    add rectangle class inherited from the base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """instatiation function"""
         self.__width = width
         self.__height = height
         self.__x = x
@@ -14,32 +16,40 @@ class Rectangle(Base):
 
         @property
         def width(self):
+            """width getter"""
             return self.__width
 
         @width.setter
         def width(self, width):
+            """width setter"""
             self.__width = width
 
         @property
         def height(self):
+            """height getter"""
             return self.__height
 
         @height.setter
-        def height(self, width):
+        def height(self, height):
+            """height setter"""
             self.__height = height
 
         @property
         def x(self):
+            """x getter"""
             return self.__x
 
         @x.setter
-        def x(self, width):
+        def x(self, x):
+            """x setter"""
             self.__x = x
 
         @property
         def y(self):
+            """y getter"""
             return self.__y
 
         @y.setter
-        def width(self, width):
+        def y(self, y):
+            """y setter"""
             self.__y = y
