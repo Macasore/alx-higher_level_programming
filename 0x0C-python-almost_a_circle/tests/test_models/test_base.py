a1f337217e8a8efcf41a776590e76c31ff29ac41
Macasore <macasorekingdavid@gmail.com>
2023-04-18 09:29:12 +0100

    fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,11 +11,25 @@ class TestBase(unittest.TestCase):
         expected i.e initialises object with id if specified
         or defaults to number of instances with default ids
         """
+        base_0 = Base()
         base_1 = Base(1)
         base_2 = Base(8)
         base_3 = Base(2)
 
         # Assert their id values
+        self.assertEqual(base_0.id, 1)
         self.assertEqual(base_1.id, 1)
         self.assertEqual(base_2.id, 8)
         self.assertEqual(base_3.id, 2)
+
+
+    def test_idnone(self):
+        """test_idnone for empty id argument"""
+        base_4 = Base()
+        base_6 = Base()
+        self.assertEqual(base_4.id, 2)
+        self.assertEqual(base_6.id, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
