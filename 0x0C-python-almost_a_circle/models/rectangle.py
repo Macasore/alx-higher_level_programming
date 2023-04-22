9c54fcd08eb5bc1d13c023437ad038cc7d5b06a4
Macasore <macasorekingdavid@gmail.com>
2023-04-22 05:20:59 +0100

    feat: update rectangle class with display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,114 +1,164 @@
 #!/usr/bin/python3
-"""rectangle module"""
+""" Rectangle Module
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class
-     Defines a rectangle object with
+    """Rectangle Class
+    Defines a rectangle object with
     width, height and x, y coordinates
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """instatiation function
-        Constructor
+        """ Constructor
         Call parent class constructor and initialises
         width, height, x and y values
-
         """
-        self.width = width
-        self.height = height
+        super().__init__(id)
         self.x = x
         self.y = y
-        super().__init__(id)
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """width getter"""
+        """Getter function for __width property
+        """
         return self.__width
 
     @width.setter
-    def width(self, val):
-        """width setter"""
-        if type(val) != int:
+    def width(self, value):
+        """Setter function for __width property
+        Validates width value
+        """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if val <= 0:
+
+        if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = val
+
+        self.__width = value
 
     @property
     def height(self):
-        """height getter"""
+        """Getter function for __height property
+        """
         return self.__height
 
     @height.setter
-    def height(self, hei):
-        """height setter"""
-        if type(hei) != int:
+    def height(self, value):
+        """Setter function for __height property
+        Validates and sets the height value
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if hei <= 0:
+
+        if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = hei
+
+        self.__height = value
 
     @property
     def x(self):
-        """x getter"""
+        """Getter function for __x property
+        """
         return self.__x
 
     @x.setter
-    def x(self, val):
-        """x setter"""
-        if type(val) != int:
+    def x(self, value):
+        """Setter function for __x property
+        Validates x value
+        """
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if val < 0:
+
+        if value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = val
+
+        self.__x = value
 
     @property
     def y(self):
-        """y getter"""
+        """Getter function for __y property
+        """
         return self.__y
 
     @y.setter
-    def y(self, te):
-        """y setter"""
-        if type(te) != int:
+    def y(self, value):
+        """Setter function for __y property
+        Validates y value
+        """
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if te < 0:
+
+        if value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = te
+
+        self.__y = value
+
+    # Public instance Methods
 
     def area(self):
-        """this area function is for calculating
-        the area of the rectangle
+        """area function
+        Returns the area of the rectangle instance
         """
+
         return self.__width * self.__height
 
     def display(self):
-        """display
-        prints the rectangle instance with the # character
+        """display function
+        Prints the rectangle to stdout using #
         """
-        if self.__y > 0:
-            for i in range(self.__y):
-                print()
-        for i in range(self.height):
-            if self.__x > 0:
-                print(" " * self.__x, end="")
-            print("#" * self.__width)
 
-    def update(self, *args):
-        if len(args) == 1:
-            for i in args:
-                self.id = i
-        elif len(args) == 2:
-            self.id, self.__width = args
-        elif len(args) == 3:
-            self.id, self.__width, self.__height = args
-        elif len(args) == 4:
-            self.id, self.__width, self.__height, self.__x = args
-        else:
-            self.id, self.__width, self.__height, self.__x, self.__y = args
+        # Single row of entire rectangle
+        row = f"{self.__x * ' '}{self.__width * '#'}\n"
 
-    def __str__(self):
-        return ("[Rectangle] ("+str(self.id)+") "+str(self.__x)+"/"
-                + str(self.__y)+" - "+str(self.__width)+"/"
-                + str(self.__height))
+        # Full rectangle with position factored in
+        result = "{}{}".format(self.__y * "\n", row * self.__height)
+
+        print(result, end="")
+
+    def __str__(self) -> str:
+        """string representation function
+        """
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """update function
+        Updates rectangle attributes in the order
+        id, width, height, x, y
+        keyword args are also allowed
+        """
+        # List of instance attributes in the order they are passed as *args
+        attr_keys = ["id", "width", "height", "x", "y"]
+
+        # Set attributes based on the no keyword args
+        for index, arg in enumerate(args):
+            self.__setattr__(attr_keys[index], arg)
+
+        # If args is not empty, skip kwargs
+        if len(args) != 0:
+            return
+
+        for key, value in kwargs.items():
+            if key in attr_keys:
+                self.__setattr__(key, value)
+
+    def to_dictionary(self):
+        """to_dictionary function
+        Converts rectangle instance to a
+        dictionary representation of the instance
+        """
+        # Attribute keys
+        attr_keys = ["id", "width", "height", "x", "y"]
+
+        # Define new dictionary and assign keys with the corresponding values
+        dic = {}
+
+        for key in attr_keys:
+            dic[key] = self.__getattribute__(key)
+
+        return dic
