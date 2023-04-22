90d2f0ee5e97f6725ac18e6e0a6fd06061154281
Macasore <macasorekingdavid@gmail.com>
2023-04-22 04:01:50 +0100

    feat: update rectangle class with display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,10 @@ class Rectangle(Base):
         """display
         prints the rectangle instance with the # character
         """
+        for i in range(self.__y):
+            print()
         for i in range(self.height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def __str__(self):
