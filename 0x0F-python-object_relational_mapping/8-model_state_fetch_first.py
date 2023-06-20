af12b39a980ab7d97e928860b8685bcdd11a7a49
Macasore <macasorekingdavid@gmail.com>
2023-06-21 00:59:20 +0100

    solution for task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
-        print("Nothing")
-        print()
+        print("Nothing\n")
 
     session.close()
