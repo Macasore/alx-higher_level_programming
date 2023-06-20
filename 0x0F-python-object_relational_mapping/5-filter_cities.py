9112db89569e7cb3c65b78d4f55584908a7b5493
Macasore <macasorekingdavid@gmail.com>
2023-06-20 02:29:06 +0100

    feat: add task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     state_name = sys.argv[4]
     query = "SELECT name from cities WHERE cities.state_id\
-        =(SELECT id from states where name=%s) ORDER BY cities.id"
+        =(SELECT id from states where name=%s) ORDER BY cities.id ASC"
     cur.execute(query, (state_name, ))
     rows = cur.fetchall()
     for row in rows:
